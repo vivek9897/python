1116028ec7d1c8863678f6fccdef42c585b9da0b
vivek <52064819+vivek9897@users.noreply.github.com>
2019-07-10 12:13:37 +0530

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,81 @@
+######classes
+# class student:
+#      def __init__(self, name="python"):
+#          self.name=name
+#          print("hello world")
+#      def welcome(self):
+#          print(self.name)
+#          print("welcome to python programming")
+# obj = student("viv")
+# obj.welcome()
+# obj1 = student()
+# obj.welcome
+
+######################
+
+# class student:
+#     def __init__(self,name,address="dehradun",phone=0):
+#         self.name=name
+#         self.address=address
+#         self.phone=phone
+#         print("welcome to brillica services")
+#     def display_data(self):
+#         print("printing students detsils")
+#         print(self.name)
+#         print(self.address)
+#         print(self.phone)
+# name = input("enter your name")
+# address = input("enter your address")
+# phone = input("enter phone no.")
+#
+# if((len(address)>1)& (len(phone)>1)):
+#     phone = int(phone)
+#     obj = student(name= name,address=address,phone=phone)
+#     obj.display_data()
+# elif((len(address)>1)&(len(phone)==0)):
+#     obj = student(name,address)
+#     obj.display_data()
+# elif ((len(address)==0) & (len(phone)>0)):
+#     obj = student(name=name,phone=phone)
+#     obj.display_data()
+# else:
+#     obj=student(name)
+#     obj.display_data()
+
+
+####################
+# class student:
+#     def __init__(self,name,address="dehradun",phone=0):
+#         self.name=name
+#         self.address=address
+#         self.phone=phone
+#         print("welcome to brillica services")
+#     def display_data(self):
+#         print("printing students detsils")
+#         print(self.name)
+#         print(self.address)
+#         print(self.phone)
+#
+# entries=int(input("enter no. of enteries"))
+# e1 =[]
+# for i in range(entries):
+#     name = input("enter your name")
+#     address = input("enter your address")
+#     phone = input("enter phone no.")
+# obj:student
+#
+# if ((len(address) > 1) & (len(phone) > 1)):
+#     phone = int(phone)
+#     obj = student(name=name, address=address, phone=phone)
+# elif ((len(address) > 1) & (len(phone) == 0)):
+#     obj = student(name, address)
+# elif ((len(address) == 0) & (len(phone) > 0)):
+#     obj = student(name=name, phone=phone)
+#     phone = int(phone)
+#
+# else:
+#     obj = student(name)
+# e1.append=(obj)
+# for i in e1:
+#     i.display_data()
+
