5d7beaae1ff0d097d56cfc2bea689307f159e621
vivek <52064819+vivek9897@users.noreply.github.com>
2019-07-10 12:15:19 +0530

    Add files via upload

---
+++ databases.py
@@ -0,0 +1,93 @@
+# import sqlite3
+#
+# connection = sqlite3.connect('student.db') # file name
+# print("Database opened successfully.")
+#
+# TABLE_NAME = "student_table"
+# STUDENT_ID = "student_id"
+# STUDENT_NAME = "student_name"
+# STUDENT_COLLEGE = "student_college"
+# STUDENT_ADDRESS = "student_address"
+# STUDENT_PHONE = "student_phone"
+#
+# connection.execute(" CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " ( " + STUDENT_ID +
+#                    " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+#                    STUDENT_NAME + " TEXT, " + STUDENT_COLLEGE + " TEXT, " +
+#                    STUDENT_ADDRESS + " TEXT, " + STUDENT_PHONE + " INTEGER );")
+#
+# print("table created successfully.")
+#
+# ## insert new record
+# # connection.execute("INSERT INTO " + TABLE_NAME + " VALUES ( 4, 'Pramod', 'DIT', 'Dehradun, Uttrakhand', 9810821705 ); " )
+# connection.execute("INSERT INTO " + TABLE_NAME + " ( " + STUDENT_NAME + ", " +
+#                    STUDENT_COLLEGE + ", " + STUDENT_ADDRESS + ", " +
+#                     STUDENT_PHONE + " ) VALUES ( 'Gurjas', 'DIT', "
+#                                     "'Kapurthala, Punjab', 9810821705 ); ")
+# connection.commit()
+#
+# ## Dynamically adding values to database
+# for i in range(2):
+#
+#     name = input('Enter your name')
+#     college = input('Enter your college')
+#     address = input('Enter your address')
+#     phone = input('Enter your phone number')
+#
+#     connection.execute("INSERT INTO " + TABLE_NAME + " ( " +
+#                       STUDENT_NAME + ", " +
+#                       STUDENT_COLLEGE + ", " + STUDENT_ADDRESS +
+#                       ", " + STUDENT_PHONE +
+#                       " ) VALUES ( '"+name+"', '"+college+"', " +
+#                        "'"+address+"', "+phone+" ); ")
+# #     connection.commit()
+
+# retrieve record
+# cursor = connection.execute("SELECT * FROM " + TABLE_NAME + " ;")
+#
+# for row in cursor:
+#     print("Student id is: ", row[0])
+#     print("Student name is: ", row[1])
+#     print("Student college is: ", row[2])
+#
+# connection.close()
+
+
+######database management
+
+import sqlite3
+connection = sqlite3.connect('management.db')
+print("database connected succesfully")
+TABLE_NAME = "MANAGEMENT_TABLE"
+STUDENT_ID = "student_id"
+STUDENT_NAME = "student_name"
+STUDENT_COLLEGE = "student_college"
+STUDENT_ADDRESS = "student_address"
+STUDENT_PHONE = "student_phone"
+connection.execute( " CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " ( "
+                     + STUDENT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                     + STUDENT_NAME + " TEXT, " + STUDENT_COLLEGE + " TEXT, " + STUDENT_ADDRESS + " TEXT, " + STUDENT_PHONE + " INTEGER );")
+print("table created succesfully")
+connection.execute("INSERT INTO " + TABLE_NAME + " ( " + STUDENT_NAME + ", " +
+                   STUDENT_COLLEGE + ", " + STUDENT_ADDRESS + ", " +
+                    STUDENT_PHONE + " ) VALUES ( 'viv', 'DIT', "
+                                    "'Kapurthala, Punjab', 9897136104 ); ")
+for i in range(2):
+    NAME = input("enter your name")
+    COLLEGE = input("enter college name")
+    ADDRESS = input("enter your address")
+    PHONE = input("enter phone no.")
+
+    connection.execute( " INSERT INTO " + TABLE_NAME + " ( " + STUDENT_NAME + " , " + STUDENT_COLLEGE + " , "
+                       + STUDENT_ADDRESS + " , " + STUDENT_PHONE + "  ) VALUES ( '" + NAME + "', '" + COLLEGE + "', " +
+                       "'" + ADDRESS + "', " + PHONE + " ); ")
+    connection.commit()
+
+#retrieve record
+cursor = connection.execute("SELECT * FROM " + TABLE_NAME + " ;")
+
+for row in cursor:
+    print("Student id is: ", row[0])
+    print("Student name is: ", row[1])
+    print("Student college is: ", row[2])
+
+connection.close()
